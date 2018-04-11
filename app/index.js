114791f72830731a24257d99f5ea0a2062add719
Mark Steadman <mark@bloomsbury.digital>
2018-04-11 19:17:09 +0100

    Adds static file serving and better 404 handling

--- app/index.js
+++ app/index.js
@@ -1,20 +1,41 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 var url = require('url');
 var routes = require('./routes');
 var RequestContext = require('./requestContext');
 
 http.createServer(
     function(request, response) {
-        var path = url.parse(request.url).pathname;
+        var requestPath = url.parse(request.url).pathname;
         var query = url.parse(request.url).query;
         var context = new RequestContext();
         var viewResponse = false;
+        var notFound = function() {
+            response.writeHead(
+                404,
+                {
+                    'Content-Type': 'text/html'
+                }
+            );
 
+            response.write('<html><head>');
+            response.write('<title>Internal server error</title>');
+            response.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
+            response.write('<style>*{line-height:1.2;margin:0}html{color:#888;display:table;font-family:sans-serif;height:100%;text-align:center;width:100%}body{display:table-cell;vertical-align:middle;margin:2em auto}h1{color:#555;font-size:2em;font-weight:400}p{margin:0 auto;width:380px}a{color:inherit}@media only screen and (max-width:280px){body,p{width:95%}h1{font-size:1.5em;margin:0 0 .3em}}</style>');
+            response.write('</head><body>');
+            response.write('<h1>Page not found</h1>');
+            response.write('<p>Check you typed the right URL or <a href="/">go back to the homepage</a>.</p>');
+            response.write('</body></html>');
+            response.end();
+        };
+
+        console.debug(request.method, request.url);
         Object.keys(routes).forEach(
             function(routePath) {
                 var formattedEx = routePath.replace(/\:([\w-]+)/, '([^/]+)');
                 var pathEx = new RegExp('^' + formattedEx + '$');
-                var matches = path.match(pathEx);
+                var matches = requestPath.match(pathEx);
                 var args = [];
                 var func = routes[routePath];
 
@@ -73,19 +94,58 @@ http.createServer(
                     console.error(err);
                 }
             );
-        } else {
-            response.writeHead(
-                404,
-                {
-                    'Content-Type': 'text/html'
+
+            return;
+        }
+
+        if(requestPath.substr(0, '/static/'.length) == '/static/') {
+            var extname = path.extname(requestPath);
+
+            fs.readFile(
+                path.join(__dirname, '../' + requestPath),
+                'utf8',
+                function(err, data) {
+                    if(err) {
+                        console.error(err);
+                        notFound();
+                        return;
+                    }
+
+                    var contentType = 'application/octet-stream';
+
+                    switch (extname) {
+                        case '.js':
+                            contentType = 'text/javascript';
+                            break;
+                        case '.css':
+                            contentType = 'text/css';
+                            break;
+                        case '.png':
+                            contentType = 'image/png';
+                            break;
+                        case '.jpg':
+                            contentType = 'image/jpg';
+                            break;
+                    }
+
+                    response.writeHead(
+                        200,
+                        {
+                            'Content-Type': contentType
+                        }
+                    );
+
+                    response.write(data);
+                    response.end();
                 }
             );
 
-            response.write('Page not found.');
-            response.end();
+            return;
         }
+
+        notFound();
     }
 ).listen(8000);
 
-console.log();
-console.log('Development server now listening at http://0.0.0.0:8000/');
+console.info();
+console.info('Development server now listening at http://0.0.0.0:8000/');
