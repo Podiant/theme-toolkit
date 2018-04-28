7f4db062252704c87d495726af57c8e4287e5518
Mark Steadman <mark@bloomsbury.digital>
2018-04-28 12:15:54 +0100

    Adds network URLs

---
+++ app/routes/network.js
@@ -0,0 +1,125 @@
+var Paginator = require('../utils/paginator');
+
+module.exports = {
+    '/': function() {
+        var self = this;
+        var page = self.GET.page ? parseInt(self.GET.page) : 1;
+
+        return self.data('network/podcasts').then(
+            function(context) {
+                return self.template('podcast_list', context);
+            }
+        );
+    },
+    '/:podcast/e/:slug/': function(podcast, slug) {
+        var self = this;
+
+        return self.data('network/episodes/' + slug).then(
+            function(context) {
+                return self.template('episode_detail', context);
+            }
+        );
+    },
+    '/iframe/': function(slug) {
+        return this.static('iframe.html');
+    },
+    '/button/': function(slug) {
+        return this.static('button.html');
+    },
+    '/app.js': function(slug) {
+        return this.static('app.js');
+    },
+    '/404/': function(slug) {
+        return this.template('page_not_found');
+    },
+    '/hosts/': function(slug) {
+        var self = this;
+
+        return self.data('network/hosts').then(
+            function(context) {
+                return self.template('host_list', context);
+            }
+        );
+    },
+    '/search/': function(slug) {
+        var self = this;
+        var page = self.GET.page ? parseInt(self.GET.page) : 1;
+
+        return self.data('network/search_results').then(
+            function(context) {
+                context.page_obj = new Paginator(context.object_list, page);
+                return self.template('search_list', context);
+            }
+        );
+    },
+    '/:podcast/': function(podcast) {
+        var self = this;
+        var page = self.GET.page ? parseInt(self.GET.page) : 1;
+        var template = page > 1 ? 'episode_list' : 'home';
+
+        return self.data('network/podcast').then(
+            function(context) {
+                context.page_obj = new Paginator(context.object_list, page);
+                return self.template(template, context);
+            }
+        );
+    },
+    '/:podcast/b/': function(podcast) {
+        var self = this;
+        var page = self.GET.page ? parseInt(self.GET.page) : 1;
+
+        return self.data('network/blog_posts').then(
+            function(context) {
+                context.page_obj = new Paginator(context.object_list, page);
+                return self.template('blogpost_list', context);
+            }
+        );
+    },
+    '/:podcast/b/:slug/': function(podcast, slug) {
+        var self = this;
+
+        return self.data('network/blog/' + slug).then(
+            function(context) {
+                return self.template('blogpost_detail', context);
+            }
+        );
+    },
+    '/:podcast/g/': function(podcast) {
+        var self = this;
+        var page = self.GET.page ? parseInt(self.GET.page) : 1;
+
+        return self.data('network/guests').then(
+            function(context) {
+                context.page_obj = new Paginator(context.object_list, page);
+                return self.template('guest_list', context);
+            }
+        );
+    },
+    '/:podcast/g/:slug/': function(podcast, slug) {
+        var self = this;
+
+        return self.data('network/guests/' + slug).then(
+            function(context) {
+                return self.template('guest_detail', context);
+            }
+        );
+    },
+    '/:podcast/about/': function(podcast, slug) {
+        var self = this;
+
+        return self.data('network/pages/about').then(
+            function(context) {
+                return self.template('page_detail', context);
+            }
+        );
+    },
+    '/:podcast/hosts/': function(podcast) {
+        var self = this;
+
+        return self.data('network/hosts').then(
+            function(context) {
+                return self.template('host_list', context);
+            }
+        );
+    }
+};
